8826c665969ced7ffb2422e9aeb8c5e58e55effd
plubeda <plubeda@ujaen.es>
2021-02-02 09:56:22 +0100

    Code for replicability

---
+++ src/utils.py
@@ -0,0 +1,193 @@
+from __future__ import absolute_import, division, print_function
+
+import csv
+import logging
+import os
+import sys
+from io import open
+
+from scipy.stats import pearsonr, spearmanr
+from sklearn.metrics import matthews_corrcoef, f1_score
+
+from multiprocessing import Pool, cpu_count
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text_a, text_b=None, label=None):
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids, label_id):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+
+
+class DataProcessor(object):
+    """Base class for data converters for sequence classification data sets."""
+
+    def get_train_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the train set."""
+        raise NotImplementedError()
+
+    def get_dev_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the dev set."""
+        raise NotImplementedError()
+
+    def get_labels(self):
+        """Gets the list of labels for this data set."""
+        raise NotImplementedError()
+
+    @classmethod
+    def _read_tsv(cls, input_file, quotechar=None):
+        """Reads a tab separated value file."""
+        with open(input_file, "r", encoding="utf-8-sig") as f:
+            reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
+            lines = []
+            for line in reader:
+                if sys.version_info[0] == 2:
+                    line = list(unicode(cell, 'utf-8') for cell in line)
+                lines.append(line)
+            return lines
+
+
+class BinaryProcessor(DataProcessor):
+    """Processor for the binary data sets"""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            text_a = line[3]
+            label = line[1]
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
+
+def convert_example_to_feature(example_row, pad_token=0,
+sequence_a_segment_id=0, sequence_b_segment_id=1,
+cls_token_segment_id=1, pad_token_segment_id=0,
+mask_padding_with_zero=True, sep_token_extra=False):
+    example, label_map, max_seq_length, tokenizer, output_mode, cls_token_at_end, cls_token, sep_token, cls_token_segment_id, pad_on_left, pad_token_segment_id, sep_token_extra = example_row
+
+    tokens_a = tokenizer.tokenize(example.text_a)
+
+    tokens_b = None
+    if example.text_b:
+        tokens_b = tokenizer.tokenize(example.text_b)
+        special_tokens_count = 4 if sep_token_extra else 3
+        _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - special_tokens_count)
+    else:
+        special_tokens_count = 3 if sep_token_extra else 2
+        if len(tokens_a) > max_seq_length - special_tokens_count:
+            tokens_a = tokens_a[:(max_seq_length - special_tokens_count)]
+
+    
+    tokens = tokens_a + [sep_token]
+    segment_ids = [sequence_a_segment_id] * len(tokens)
+
+    if tokens_b:
+        tokens += tokens_b + [sep_token]
+        segment_ids += [sequence_b_segment_id] * (len(tokens_b) + 1)
+
+    if cls_token_at_end:
+        tokens = tokens + [cls_token]
+        segment_ids = segment_ids + [cls_token_segment_id]
+    else:
+        tokens = [cls_token] + tokens
+        segment_ids = [cls_token_segment_id] + segment_ids
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    input_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
+
+    padding_length = max_seq_length - len(input_ids)
+    if pad_on_left:
+        input_ids = ([pad_token] * padding_length) + input_ids
+        input_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + input_mask
+        segment_ids = ([pad_token_segment_id] * padding_length) + segment_ids
+    else:
+        input_ids = input_ids + ([pad_token] * padding_length)
+        input_mask = input_mask + ([0 if mask_padding_with_zero else 1] * padding_length)
+        segment_ids = segment_ids + ([pad_token_segment_id] * padding_length)
+
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+
+    if output_mode == "classification":
+        label_id = label_map[example.label]
+    elif output_mode == "regression":
+        label_id = float(example.label)
+    else:
+        raise KeyError(output_mode)
+
+    return InputFeatures(input_ids=input_ids,
+                        input_mask=input_mask,
+                        segment_ids=segment_ids,
+                        label_id=label_id)
+    
+
+def convert_examples_to_features(examples, label_list, max_seq_length,
+                                 tokenizer, output_mode,
+                                 cls_token_at_end=False, sep_token_extra=False, pad_on_left=False,
+                                 cls_token='[CLS]', sep_token='[SEP]', pad_token=0,
+                                 sequence_a_segment_id=0, sequence_b_segment_id=1,
+                                 cls_token_segment_id=1, pad_token_segment_id=0,
+                                 mask_padding_with_zero=True,
+                                 process_count=cpu_count()):
+    
+    label_map = {label : i for i, label in enumerate(label_list)}
+
+    examples = [(example, label_map, max_seq_length, tokenizer, output_mode, cls_token_at_end, cls_token, sep_token, cls_token_segment_id, pad_on_left, pad_token_segment_id, sep_token_extra) for example in examples]
+    with Pool(process_count) as p:
+        features = list(tqdm(p.imap(convert_example_to_feature, examples, chunksize=500), total=len(examples)))
+
+    return features
+
+
+def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+    
+    while True:
+        total_length = len(tokens_a) + len(tokens_b)
+        if total_length <= max_length:
+            break
+        if len(tokens_a) > len(tokens_b):
+            tokens_a.pop()
+        else:
+            tokens_b.pop()
+
+
+processors = {
+    "binary": BinaryProcessor
+}
+
+output_modes = {
+    "binary": "classification"
+}
